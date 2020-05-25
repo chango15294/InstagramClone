e11bfa47f8227416802caed6f0797272328cd1ac
chango15 <harri1515@hotmail.com>
2020-05-25 12:08:34 +0200

    Implementando User Tab ( tabla usuarios)

--- app/src/main/java/com/example/harri/instagramclone/ProfileTab.java
+++ app/src/main/java/com/example/harri/instagramclone/ProfileTab.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
+
+    private EditText edtProfileName, edtProfileBio, edtProfileProfession,
+                    edtProfileHobbies, edtProfilefavSport;
+    private Button btnUpdateInfo;
 
 
     public ProfileTab() {
@@ -23,7 +35,96 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfilefavSport = view.findViewById(R.id.edtProfileFavoriteSport);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if(parseUser.get("profileName") == null)
+        {
+            edtProfileName.setText("");
+        }else
+        {
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+
+        if(parseUser.get("profileBio") == null)
+        {
+            edtProfileBio.setText("");
+        }else
+        {
+            edtProfileBio.setText(parseUser.get("profileBio").toString());
+        }
+
+        if(parseUser.get("profileProfession") == null)
+        {
+            edtProfileBio.setText("");
+        }else
+        {
+            edtProfileBio.setText(parseUser.get("profileProfession").toString());
+        }
+
+        if(parseUser.get("profileHobbies") == null)
+        {
+            edtProfileBio.setText("");
+        }else
+        {
+            edtProfileBio.setText(parseUser.get("profileHobbies").toString());
+        }
+
+        if(parseUser.get("profileFavSpor") == null)
+        {
+            edtProfileBio.setText("");
+        }else
+        {
+            edtProfileBio.setText(parseUser.get("profileFavSpor").toString());
+        }
+
+
+
+
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                parseUser.put("profileName", edtProfileName.getText()+ "");
+                parseUser.put("profileBio", edtProfileBio.getText()+ "");
+                parseUser.put("profileProfession", edtProfileProfession.getText() + "");
+                parseUser.put("profileHobbies",edtProfileHobbies.getText() + "");
+                parseUser.put("profileFavSport",edtProfilefavSport.getText() + "");
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e)
+                    {
+                      if( e == null)
+                      {
+                          FancyToast.makeText(getContext(),
+                                   " Información actualizada",
+                                  Toast.LENGTH_LONG,FancyToast.SUCCESS,
+                                  true).show();
+                      }
+                      else
+                      {
+                          FancyToast.makeText(getContext(),
+                                  e.getMessage(),
+                                  Toast.LENGTH_LONG,FancyToast.ERROR,
+                                  true).show();
+                      }
+                    }
+                });
+            }
+        });
+
+        return view;
     }
 
 }
